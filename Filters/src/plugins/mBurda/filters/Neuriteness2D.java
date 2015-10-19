25391a6e953d5de48a0588ff11d616abbe2baf2b
Marek Burda <m.burda583@gmail.com>
2015-10-19 17:38:07 +0200

    uprava 2D suborov

--- Filters/src/plugins/mBurda/filters/Neuriteness2D.java
+++ Filters/src/plugins/mBurda/filters/Neuriteness2D.java
@@ -86,20 +86,20 @@ public class Neuriteness2D{
 			for(int x=0;x<source.getWidth();x++){
 				if(x+1<source.getWidth() && x-1>=0){
 					//Horizontal approximation
-					double a = (raster.getSample(x+1, y, 0)+raster.getSample(x-1, y, 0)-2*raster.getSample(x, y, 0));
-					hessian.set(0, 0, a*phaseCong[x][y]); 	
+					double a = (raster.getSample(x+1, y, 0)*phaseCong[x+1][y]+raster.getSample(x-1, y, 0)*phaseCong[x-1][y]-2*raster.getSample(x, y, 0)*phaseCong[x][y]);
+					hessian.set(0, 0, a); 	
 				} else hessian.set(0, 0, 0);
 				if(y+1 < source.getHeight() && y-1>=0)
 				{
 					//Vertical approximation
-					double a = (raster.getSample(x, y+1, 0)+raster.getSample(x, y-1, 0)-2*raster.getSample(x, y, 0));
-						hessian.set(1, 1, a*phaseCong[x][y]);
+					double a = (raster.getSample(x, y+1, 0)*phaseCong[x][y+1]+raster.getSample(x, y-1, 0)*phaseCong[x][y-1]-2*phaseCong[x][y]*raster.getSample(x, y, 0));
+						hessian.set(1, 1, a);
 				} else hessian.set(1, 1, 0);
 				if(x+1<source.getWidth() && x-1>=0 && y+1 < source.getHeight() && y-1>=0)
 				{
 					//Diagonal approximation
-					double a = (raster.getSample(x+1, y+1, 0)+raster.getSample(x-1, y-1, 0)-raster.getSample(x+1, y-1, 0)-raster.getSample(x-1, y+1, 0))/4;
-						hessian.set(0,1,a*phaseCong[x][y]);
+					double a = (raster.getSample(x+1, y+1, 0)*phaseCong[x+1][y+1]+raster.getSample(x-1, y-1, 0)*phaseCong[x-1][y-1]-raster.getSample(x+1, y-1, 0)*phaseCong[x+1][y-1]-raster.getSample(x-1, y+1, 0)*phaseCong[x-1][y+1])/4;
+						hessian.set(0,1,a);
 				} else hessian.set(0, 1, 0);
 				hessian.set(1, 0, hessian.get(0,1));
 				
--- Filters/src/plugins/mBurda/filters/Vesselness2D.java
+++ Filters/src/plugins/mBurda/filters/Vesselness2D.java
@@ -89,20 +89,20 @@ public class Vesselness2D{
 			for(int x=0;x<source.getWidth();x++){
 				if(x+1<source.getWidth() && x-1>=0){
 					//Horizontal approximation
-					double a = (raster.getSample(x+1, y, 0)+raster.getSample(x-1, y, 0)-2*raster.getSample(x, y, 0));
-					hessian.set(0, 0, a*phaseCong[x][y]); 	
+					double a = (raster.getSample(x+1, y, 0)*phaseCong[x+1][y]+raster.getSample(x-1, y, 0)*phaseCong[x-1][y]-2*raster.getSample(x, y, 0)*phaseCong[x][y]);
+					hessian.set(0, 0, a/**phaseCong[x][y]*/); 	
 				} else hessian.set(0, 0, 0);
 				if(y+1 < source.getHeight() && y-1>=0)
 				{
 					//Vertical approximation
-					double a = (raster.getSample(x, y+1, 0)+raster.getSample(x, y-1, 0)-2*raster.getSample(x, y, 0));
-						hessian.set(1, 1, a*phaseCong[x][y]);
+					double a = (raster.getSample(x, y+1, 0)*phaseCong[x][y+1]+raster.getSample(x, y-1, 0)*phaseCong[x][y-1]-2*raster.getSample(x, y, 0)*phaseCong[x][y]);
+						hessian.set(1, 1, a/**phaseCong[x][y]*/);
 				} else hessian.set(1, 1, 0);
 				if(x+1<source.getWidth() && x-1>=0 && y+1 < source.getHeight() && y-1>=0)
 				{
 					//Diagonal approximation
-					double a = (raster.getSample(x+1, y+1, 0)+raster.getSample(x-1, y-1, 0)-raster.getSample(x+1, y-1, 0)-raster.getSample(x-1, y+1, 0))/4;
-						hessian.set(0,1,a*phaseCong[x][y]);
+					double a = (raster.getSample(x+1, y+1, 0)*phaseCong[x+1][y+1]+raster.getSample(x-1, y-1, 0)*phaseCong[x-1][y-1]-raster.getSample(x+1, y-1, 0)*phaseCong[x+1][y-1]-raster.getSample(x-1, y+1, 0)*phaseCong[x-1][y+1])/4;
+						hessian.set(0,1,a/**phaseCong[x][y]*/);
 				} else hessian.set(0, 1, 0);
 				hessian.set(1, 0, hessian.get(0,1));
 				
@@ -119,7 +119,7 @@ public class Vesselness2D{
 				double disparsity = Math.exp(-((eig1*eig1)/(2*beta*beta*eig2*eig2)));
 				double relativeBrightness = (1 - Math.exp(-((eig1*eig1+eig2*eig2)/2*brightThresh*brightThresh)));
 				if(eig2 < 0) this.vesselness2D[x][y] = 0;
-				else this.vesselness2D[x][y] =  disparsity * relativeBrightness;
+				else this.vesselness2D[x][y] =  disparsity * relativeBrightness/* * phaseCong[y][x]*/;
 				}
 			}
 	}
